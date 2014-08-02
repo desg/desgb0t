d4a23a6a18f65446e41bafdf304a4f2e755937e9
Demetrius M <desg@xdesg.net>
2014-08-02 17:57:39 -0400

    add trole command

---
+++ plugins/trole.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def trole(ircclientinstance,serverbuffer):
+	commandname = 'trole'
+	commands = {':!' : True, ':.' : False, ':@' : True}
+	output = "( ͡° ͜ʖ ͡°)"
+
+	if serverbuffer[3][2:] == commandname:
+		if serverbuffer[3][:2] in commands:
+			notice = commands.get(serverbuffer[3][:2])
+
+			if not notice:
+				if serverbuffer[1] == "privmsg":
+						if serverbuffer[2] in ircclientinstance.ircchanlist:
+							ircclientinstance.sendmessage(serverbuffer[2],output)
+						else:
+							ircclientinstance.sendmessage(ircclientinstance.getusernick(serverbuffer),output)
+			else:
+				ircclientinstance.sendnotice(ircclientinstance.getusernick(serverbuffer),output)
