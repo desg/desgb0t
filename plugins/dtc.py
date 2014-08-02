42842348e75dacd609ed23d2bd5db4980cebe77e
Demetrius M <desg@xdesg.net>
2014-08-02 17:55:39 -0400

    add dtc command

---
+++ plugins/dtc.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+def dtc(ircclientinstance,serverbuffer):
+	commandname = 'dtc'
+	commands = {':!' : True, ':.' : False, ':@' : True}
+
+	today = date.today()
+	christmas = date(today.year, 12, 25)
+
+	if christmas < today:
+		christmas = christmas.replace(year=today.year + 1)
+
+	tUntilChristmas = abs(christmas - today)
+
+	output = "%i days until Christmas!" % tUntilChristmas.days
+
+	if serverbuffer[3][2:] == commandname:
+		if serverbuffer[3][:2] in commands:
+			notice = commands.get(serverbuffer[3][:2])
+
+			if not notice:
+				if serverbuffer[1] == "privmsg":
+						if serverbuffer[2] in ircclientinstance.ircchanlist:
+							ircclientinstance.sendmessage(serverbuffer[2],output)
+						else:
+							ircclientinstance.sendmessage(ircclientinstance.getusernick(serverbuffer),output)
+			else:
+				ircclientinstance.sendnotice(ircclientinstance.getusernick(serverbuffer),output)
